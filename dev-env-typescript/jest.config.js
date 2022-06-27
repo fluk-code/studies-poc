bd4fec11ea316c65fa524aa36ede9af5df24579a
fluk-code <ferreirafelipericardo@gmail.com>
2022-06-27 07:49:28 -0300

    chore: add test framework (dev-env-typescript)

---
+++ dev-env-typescript/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testRegex: '.*\\..*spec\\.ts$',
+  transform: {
+    '^.+\\.ts?$': ['@swc/jest'],
+  },
+};
